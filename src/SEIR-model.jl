c025c02369b9f42a4f926f9fc847ffc352c25a25
Ruben Schlotter <ruben.schlotter@gmail.com>
2022-01-13 20:58:14 +0100

    more models

--- src/SEIR-model.jl
+++ src/SEIR-model.jl
@@ -1,17 +1,27 @@
 # SEIR Model
+
 using DifferentialEquations
 
-# This is a test for the Lorenz ODE system
-function lorenz!(du,u,p,t)
- du[1] = 10.0*(u[2]-u[1])
- du[2] = u[1]*(28.0-u[3]) - u[2]
- du[3] = u[1]*u[2] - (8/3)*u[3]
+
+beta = 0.215; gamma = 0.07; N = 7000;
+alpha = 0.25;
+
+"""SEIR Model
+Extension of the basic SIR model
+
+"""
+function SEIR!(du,u,p,t)
+    du[1] = -beta* u[1]/N*u[3]
+    du[2] = beta*u[1]/N*u[3] - alpha*u[2]
+    du[3] = alpha* u[2] - gamma*u[3]
+    du[4] = gamma*u[3]
 end
-
-
-u0 = [1.0;0.0;0.0]
-tspan = (0.0,100.0)
-prob = ODEProblem(lorenz!,u0,tspan)
-sol = solve(prob)
-
-plot(sol,vars=(1,2,3))
+   
+   
+   u0 = [N-15.0;15.0;0.0;0.0]
+   tspan = (0.0,100.0)
+   prob = ODEProblem(SEIR!,u0,tspan)
+   sol = solve(prob)
+   
+   plot(sol)
+   
--- src/SIR-X-Model.jl
+++ src/SIR-X-Model.jl
@@ -1,1 +1,28 @@
 #SIR-X Model
+
+
+using DifferentialEquations
+
+
+beta = 0.215; gamma = 0.07; N = 7000;
+eta0 = 0.003; eta = 0.003
+
+"""SIR-X Model
+Extension of the basic SIR model
+
+"""
+function SIR_X!(du,u,p,t)
+    du[1] = -beta* u[1]/N*u[2] - eta0*u[1]                          #S
+    du[2] = beta*u[1]/N*u[2] - gamma*u[2] - eta0*u[2] - eta*u[2]    #I
+    du[3] = gamma*u[2] + eta0*u[1]                                  #R
+    du[4] = (eta0 + eta)*u[2]                                       #X
+end
+   
+   
+   u0 = [N-15.0;15.0;0.0;0.0]
+   tspan = (0.0,100.0)
+   prob = ODEProblem(SIR_X!,u0,tspan)
+   sol = solve(prob)
+   
+   plot(sol)
+   
